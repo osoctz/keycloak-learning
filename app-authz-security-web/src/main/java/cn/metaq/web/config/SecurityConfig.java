3823d5aa7286c3c5d72499430cebeddcf0df10af
zantang <zantang.cn@hotmail.com>
2021-09-18 01:08:08 +0800

    纯web页面的应用

---
+++ app-authz-security-web/src/main/java/cn/metaq/web/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package cn.metaq.web.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+//    @Bean
+//    public GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+//        SimpleAuthorityMapper mapper = new SimpleAuthorityMapper();
+//        mapper.setConvertToUpperCase(true);
+//        return mapper;
+//    }
+//
+//    @Override
+//    protected KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
+//        final KeycloakAuthenticationProvider provider = super.keycloakAuthenticationProvider();
+//        provider.setGrantedAuthoritiesMapper(grantedAuthoritiesMapper());
+//        return provider;
+//    }
+
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll();
+    }
+}
---
+++ app-authz-security-web/src/main/java/cn/metaq/web/controller/IndexController.java
@@ -0,0 +1,29 @@
+package cn.metaq.web.controller;
+
+import org.keycloak.KeycloakSecurityContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/users/view", method = {RequestMethod.GET})
+    public String users_view(Model model, HttpServletRequest request) {
+
+        KeycloakSecurityContext keycloak = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
+        model.addAttribute("token", keycloak.getTokenString());
+        return "users/view";
+    }
+
+    @RequestMapping(value = "/admin/view", method = {RequestMethod.GET})
+    public String admin_view(Model model, HttpServletRequest request) {
+
+        KeycloakSecurityContext keycloak = (KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName());
+        model.addAttribute("token", keycloak.getTokenString());
+        return "admin/view";
+    }
+}
